39466988c3fb5b835cf2ae3168e11849f4bc77fa
Tandy-Nicole <84181728+Tandy-Nicole@users.noreply.github.com>
2021-09-01 20:20:24 +0100

    completed exercise B, D and E

--- exercises/E-strings-concatenation/exercise.js
+++ exercises/E-strings-concatenation/exercise.js
@@ -1,3 +1,9 @@
 // Start by creating a variable `message`
 
-console.log(message);
+var greetingStart = "Hello, my name is ";
+var name = "Tandy''";
+
+var greeting = greetingStart + name;
+
+console.log(greeting); // Logs "Hello, my name is Daniel"
+
